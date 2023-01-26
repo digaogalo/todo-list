d402034bcb319057fc99dc6b6bcef639fb88140c
digaogalo <digaolimac@gmail.com>
2023-01-26 16:15:41 -0300

    final code

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -17,44 +17,46 @@ function TodoForm(props) {
     e.preventDefault();
 
     props.onSubmit({
-        id: Math.floor(Math.random() * 10000),
-        text: input 
-    })
-  setInput('')
-};
-  return (
-    <form onSubmit={handleSubmit} className="todo-form">
-      {props.edit ? (
-        <>
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+    setInput('');
+  };
+
+return (
+  <form onSubmit={handleSubmit} className='todo-form'>
+    {props.edit ? (
+      <>
         <input
-        placeholder="Update your item"
-        value={input}
-        name="text"
-        className="todo-input edit"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className="todo-button edit">
-        Update
-      </button>
+          placeholder='Update your item'
+          value={input}
+          onChange={handleChange}
+          name='text'
+          ref={inputRef}
+          className='todo-input edit'
+        />
+        <button onClick={handleSubmit} className='todo-button edit'>
+          Update
+        </button>
       </>
-      ) : (
-        <>
+    ) : (
+      <>
         <input
-        placeholder="Add a ToDo"
-        value={input}
-        name="text"
-        className="todo-input"
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button onClick={handleSubmit} className="todo-button">
-        Add Todo
-      </button>
+          placeholder='Add a todo'
+          value={input}
+          onChange={handleChange}
+          name='text'
+          className='todo-input'
+          ref={inputRef}
+        />
+        <button onClick={handleSubmit} className='todo-button'>
+          Add todo
+        </button>
       </>
-      )}
-    </form>
-  );
+    )}
+  </form>
+);
 }
+
 
 export default TodoForm;
